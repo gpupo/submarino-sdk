def538ee034cf6a0128a85a3df5525d07cd47f7b
Alexandre Rodrigues Xavier <alexandre.rodrigues.xv@gmail.com>
2015-10-27 21:30:41 -0200

    [MEBLO-5195][MEBLO-5229] Confirmação de recebimento de pedido - Chamada para o endpoint POST /order/{itemId}/confirm

--- src/Entity/Order/Manager.php
+++ src/Entity/Order/Manager.php
@@ -18,6 +18,7 @@ class Manager extends ManagerAbstract
     protected $entity = 'Order';
 
     protected $maps = [
+        'confirm'       => ['POST', '/order/{itemId}/confirm'],
         'saveStatus'    => ['PUT', '/order/{itemId}/status'],
         'findById'      => ['GET', '/order/{itemId}'],
         'fetch'         => ['GET', '/order?offset={offset}&limit={limit}&purchaseDate={purchaseDate}&store={store}&siteId={siteId}&status={status}'],
