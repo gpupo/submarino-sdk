57627627489b4d8c01f6a86c12651285b4200152
Gilmar Pupo <g@g1mr.com>
2016-06-15 15:54:42 -0300

    php-cs-fixer

--- src/Entity/Order/Payer/Payer.php
+++ src/Entity/Order/Payer/Payer.php
@@ -8,7 +8,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 namespace Gpupo\SubmarinoSdk\Entity\Order\Payer;
 
 use Gpupo\SubmarinoSdk\Entity\Order\Customer\CustomerAbstract;
