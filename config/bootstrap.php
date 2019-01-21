8783c1d0923aa554d64b3602fe43c31b1b43d6e0
Gilmar Pupo <contact@gpupo.com>
2019-01-21 11:33:37 -0200

    Add Bootstrap file, .env

---
+++ config/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of gpupo/submarino-sdk
+ * Created by Gilmar Pupo <contact@gpupo.com>
+ * For the information of copyright and license you should read the file
+ * LICENSE which is distributed with this source code.
+ * Para a informação dos direitos autorais e de licença você deve ler o arquivo
+ * LICENSE que é distribuído com este código-fonte.
+ * Para obtener la información de los derechos de autor y la licencia debe leer
+ * el archivo LICENSE que se distribuye con el código fuente.
+ * For more information, see <https://opensource.gpupo.com/>.
+ *
+ */
+
+use Symfony\Component\Dotenv\Dotenv;
+
+if (!class_exists('\Gpupo\Common\Console\Application')) {
+    require __DIR__.'/../vendor/autoload.php';
+}
+
+if (!class_exists(Dotenv::class)) {
+    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
+}
+    // load all the .env files
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
